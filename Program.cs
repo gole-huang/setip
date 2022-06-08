a859b761e882e52bf0ffdd949e1b395b626732f7
gole_huang <gole.huang@gmail.com>
2022-06-08 22:55:08 +0800

    coreclr to clr

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace MAIN
         static void Main(string[] args)
         {
             //如果存在SetIP.xlsx，则使用本地表资料，否则查找MySQL获取；
-            //SetMyIP setIP = new SetMyIP();
             MyIP myIP = new MyIP();
-            //myIP.showMember();
         }
     }
 }
